1f237af2887f489cbbc7535e34e29e6b6332cf6e
vaibhav kolli <vaibhavkolli@vaibhavs-Air.attlocal.net>
2023-05-08 12:48:41 -0500

    hash router

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import './index.css';
+import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HashRouter base="/">
+        <App />
+    </HashRouter>
+    , document.getElementById('root')
+);
